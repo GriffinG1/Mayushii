01ce0b12e978bf9509cc87a9adb9df6ae401e647
noirscape <deepnavy@waifu.club>
2018-11-21 21:35:19 +0100

    Cleanup main.py
    - Moved guild to config.py
    - Fix quotes around addons list
    - Moved cog loading outside of on_ready() <-- on_ready() can be ran multiple times.
    
    Added requirements.txt
    - Only d.py rewrite for now
    
    Added config.py.example

--- main.py
+++ main.py
@@ -3,21 +3,22 @@ import config
 
 '''A simple bot framework. Based on https://gist.github.com/noirscape/cc8e65c1c42f26af0a9b3780e161817d'''
 
-bot = commands.Bot(command_prefix='$x', description='Voting bot for Nintendo Homebrew icon contests') 
+bot = commands.Bot(command_prefix='$', description='Voting bot for Nintendo Homebrew icon contests') 
 
 addons = [
-    `addons.vote`,
+    'addons.vote',
 ]
+
+for module in addons:
+    try:
+        bot.load_extension(module)
+    except Exception as e:
+        print('Could not load module ' + module)
+        print(e)
 
 @bot.event
 async def on_ready():
-    bot.guild = bot.get_guild(196618637950451712)
-    for module in addons:
-        try:
-            bot.load_extension(module)
-        except Exception as e:
-            print('Could not load module ' + module)
-            print(e)
+    bot.guild = bot.get_guild(config.guild)
     print('Initialized on ' + bot.guild.name)
     
 @bot.command()
