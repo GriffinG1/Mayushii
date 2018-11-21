e621fe49e54fb61dfe6c6ebdf1046bdc5c73d606
Griffin <4n0nym0ush4ck3r666@gmail.com>
2018-11-21 13:18:47 -0700

    Main.py written

---
+++ main.py
@@ -0,0 +1,34 @@
+from discord.ext import commands
+import config
+
+'''A simple bot framework. Based on https://gist.github.com/noirscape/cc8e65c1c42f26af0a9b3780e161817d'''
+
+bot = commands.Bot(command_prefix='$x', description='Voting bot for Nintendo Homebrew icon contests') 
+
+addons = [
+    `addons.vote`,
+]
+
+@bot.event
+async def on_ready():
+    bot.guild = bot.get_guild(196618637950451712)
+    for module in addons:
+        try:
+            bot.load_extension(module)
+        except Exception as e:
+            print('Could not load module ' + module)
+            print(e)
+    print('Initialized on ' + bot.guild.name)
+    
+@bot.command()
+async def reload(ctx):
+    for module in addons:
+        try:
+            bot.unload_extension(module)
+            bot.load_extension(module)
+        except Exception as e:
+            await ctx.send('Could not reload module ' + module)
+            print('Could not load module ' + module)
+            print(e)
+
+bot.run(config.token)
