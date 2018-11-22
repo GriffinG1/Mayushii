2360546119e61025e203dc86ee3adde3d9d9697c
FrozenChen <brobles@umag.cl>
2018-11-22 11:43:30 -0300

    Made poll a group, added final tally to close_cmd

--- addons/vote.py
+++ addons/vote.py
@@ -24,7 +24,7 @@ class Vote:
             with open("poll.json", "w") as pollfile:
                 json.dump(self.pollcfg, pollfile)
         self.queue = asyncio.Queue()
-        
+
     async def is_poll_ongoing(ctx):
         if ctx.cog.poll_ongoing:
             return True
@@ -45,43 +45,51 @@ class Vote:
             json.dump(self.vote_list, votefile)
 
     @commands.guild_only()
-    @commands.command()
-    async def poll(self, ctx, command, link="", name="", *, options=""):
-        if command == "create" or command == "start":
-            if not self.poll_ongoing:
-                if not link or not name or not options:
-                    return await ctx.send(f"You didn't provide an argument. You should fix that. Inputted: link-{link}, name-{name}, options-{options}.")
-                vote_options = []
-                self.pollcfg["name"] = name
-                self.pollcfg["link"] = link
-                for option in options.split(" | "):
-                    vote_options.append(option)
-                self.pollcfg["options"] = vote_options
-                with open("poll.json", "w") as pollfile:
-                    json.dump(self.pollcfg, pollfile)
-                with open(f"{name}_votes.json","w") as votefile:
-                    json.dump(self.vote_list, votefile)
-                self.poll_ongoing = True
-                await ctx.send("Poll successfully created!")
-            else:
-                await ctx.send("There is poll ongoing already!")
-                
-        if command == "close" or command == "end":
-            if self.poll_ongoing:
-                # prob add evi final tally before finishing poll
-                os.makedirs("Polls", exist_ok=True)
-                with open("Polls/{}.json".format(self.pollcfg["name"]), "w") as pollbackup:
-                    json.dump(self.pollcfg, pollbackup)
-                with open("Polls/{}_votes.json".format(self.pollcfg["name"]), "w") as votesbackup:
-                    json.dump(self.vote_list, votesbackup)
-                self.pollcfg = {}
-                self.vote_list = {}
-                with open("poll.json", "w") as pollfile:
-                    json.dump(self.pollcfg, pollfile)
-                self.poll_ongoing = False
-                await ctx.send("Poll successfully closed!")
-            else:
-                await ctx.send("There is no ongoing poll!")
+    @commands.group(pass_context=True)
+    async def poll(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send('Invalid command: {0.subcommand_passed}'.format(ctx))
+
+    @poll.command(name='create', pass_context=True, alias='start')
+    # prob add evi final tally before finishing poll
+    async def create_cmd(self, ctx, link="", name="", *, options=""):
+        if not link or not name or not options:
+            return await ctx.send(
+                f"You didn't provide an argument. You should fix that. Inputted: link-{link}, name-{name}, options-{options}.")
+        vote_options = []
+        self.pollcfg["name"] = name
+        self.pollcfg["link"] = link
+        for option in options.split(" | "):
+            vote_options.append(option)
+        self.pollcfg["options"] = vote_options
+        with open("poll.json", "w") as pollfile:
+            json.dump(self.pollcfg, pollfile)
+        with open(f"{name}_votes.json", "w") as votefile:
+            json.dump(self.vote_list, votefile)
+        self.poll_ongoing = True
+        await ctx.send("Poll successfully created!")
+
+    @commands.check(is_poll_ongoing)
+    @poll.command(name='close', pass_context=True, alias='end')
+    async def close_cmd(self,ctx):
+        #maybe add it as a method named count votes
+        votes = dict.fromkeys(self.pollcfg["options"], 0)
+        for vote in self.vote_list.values():
+            if vote in votes:
+                votes[vote] += 1
+        for v in votes.items():
+            self.pollcfg[v[0]] = v[1]
+        os.makedirs("Polls", exist_ok=True)
+        with open("Polls/{}.json".format(self.pollcfg["name"]), "w") as pollbackup:
+            json.dump(self.pollcfg, pollbackup)
+        with open("Polls/{}_votes.json".format(self.pollcfg["name"]), "w") as votesbackup:
+            json.dump(self.vote_list, votesbackup)
+        self.pollcfg = {}
+        self.vote_list = {}
+        with open("poll.json", "w") as pollfile:
+            json.dump(self.pollcfg, pollfile)
+        self.poll_ongoing = False
+        await ctx.send("Poll successfully closed!")
 
     @commands.guild_only()
     @commands.command()
