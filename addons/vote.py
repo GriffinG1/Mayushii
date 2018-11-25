222e8b35c43dd2390eeb9f278c9809049ae8cfe6
Griffin <4n0nym0ush4ck3r666@gmail.com>
2018-11-24 19:56:47 -0700

    I can't recall everything in this commit. Error handling for one

--- addons/vote.py
+++ addons/vote.py
@@ -27,10 +27,11 @@ class Vote:
 
     async def is_poll_ongoing(ctx):
         if ctx.cog.poll_ongoing:
+            print("h")
             return True
         else:
-            await ctx.send("There's no poll running!")
-            return False
+            print("f")
+            raise PollException("This thing is a pain")
 
     async def process_vote(self):
         choice, ctx = await self.queue.get()
@@ -45,22 +46,29 @@ class Vote:
             json.dump(self.vote_list, votefile)
 
     @commands.guild_only()
-    @commands.group(pass_context=True)
+    @commands.group()
     async def poll(self, ctx):
         if ctx.invoked_subcommand is None:
-            await ctx.send('Invalid command: {0.subcommand_passed}'.format(ctx))
+            await ctx.send("I don't know what you want me to do. If you would like to create a poll, please do `$help poll create`. If you would like to close an ongoing poll, please do `$poll close`.")
 
-    @poll.command(name='create', pass_context=True, alias='start')
+    @poll.command(name='create', alias='start')
     # prob add evi final tally before finishing poll
     async def create_cmd(self, ctx, link="", name="", *, options=""):
-        if not link or not name or not options:
+        """To create a poll, you  must provide the link to the imgur gallery, the name for the current poll, and a list of available options split by vertical bars. Ex:\n$poll create https://imgur.com/testing testingpoll2018 A | B | C | D"""
+        if self.poll_ongoing:
+            return await ctx.send("There's already a poll running. You must stop that one first.")
+        elif not link or not name or not options:
             return await ctx.send(
-                f"You didn't provide an argument. You should fix that. Inputted: link-{link}, name-{name}, options-{options}.")
-        vote_options = []
-        self.pollcfg["name"] = name
-        self.pollcfg["link"] = link
+                f"You didn't provide an argument. You should fix that.")
+        elif not link.startswith("https://"):
+            return await ctx.send(f"Your inputted link, `{link}`, is not a valid link.")
         for option in options.split(" | "):
             vote_options.append(option)
+        vote_options = []
+        if len(vote_options) < 2:
+            return await ctx.send("You can't have a poll with only one option.")
+        self.pollcfg["name"] = name
+        self.pollcfg["link"] = link
         self.pollcfg["options"] = vote_options
         with open("poll.json", "w") as pollfile:
             json.dump(self.pollcfg, pollfile)
@@ -70,7 +78,7 @@ class Vote:
         await ctx.send("Poll successfully created!")
 
     @commands.check(is_poll_ongoing)
-    @poll.command(name='close', pass_context=True, alias='end')
+    @poll.command(name='close', alias='end')
     async def close_cmd(self,ctx):
         #maybe add it as a method named count votes
         votes = dict.fromkeys(self.pollcfg["options"], 0)
@@ -91,12 +99,19 @@ class Vote:
         self.poll_ongoing = False
         await ctx.send("Poll successfully closed!")
 
+    @poll.command()
+    @commands.check(is_poll_ongoing)
+    async def info(self, ctx):
+        embed = discord.Embed(title="Current poll")
+        embed.add_field(name="Poll name", value=self.pollcfg["name"])
+        embed.add_field(name="Link to gallery", value=self.pollcfg["link"])
+        embed.add_field(name="Options", value=" | ".join(self.pollcfg["options"]))
+        await ctx.send(embed=embed)
+
     @commands.guild_only()
     @commands.command()
     @commands.check(is_poll_ongoing)
-    async def vote(self, ctx, choice=""):
-        if not choice:
-            return await ctx.send("You forgot to make a choice! Use `$pollinfo` to find all choices, or use `$vote cancel` to clear your vote.")
+    async def vote(self, ctx, choice):
         await self.queue.put((choice, ctx)) # tuple -> immutable plus easy get
         await self.process_vote()
 
@@ -113,16 +128,9 @@ class Vote:
         for v in votes.items():
             embed.add_field(name=f"{v[0]}", value=f"{v[1]}")
         await ctx.send(embed=embed)
-    
-    @commands.guild_only()
-    @commands.command()
-    @commands.check(is_poll_ongoing)
-    async def pollinfo(self, ctx):
-        embed = discord.Embed(title="Current poll")
-        embed.add_field(name="Poll name", value=self.pollcfg["name"])
-        embed.add_field(name="Link to gallery", value=self.pollcfg["link"])
-        embed.add_field(name="Options", value=self.pollcfg["options"])
-        await ctx.send(embed=embed)
-
+        
 def setup(bot):
     bot.add_cog(Vote(bot))
+    
+class PollException(commands.errors.CommandError):
+    pass
