42acbd926c7f2f835fd9ff6af9f9e76c7cd41f17
FrozenChen <brobles@umag.cl>
2018-11-30 07:47:22 -0300

    Make Poll create close, change init and make tally (#9)
    * Make Poll create close, change init and make tally
    
    * Check if folder exist if not create it
    
    * Fix options
    
    * Clean up, votes don't delete, checks for ongoing poll
    
    * Fix bug in vote.py, add missing send in reload
    
    * Fix bugs with the check, remove auto cleanup as it's actually not clean since invoke stays, add pollinfo, formatting for tally
    
    * Fix this mess
    
    * Cleanup strings, better method for dir creation
    
    * Made poll a group, added final tally to close_cmd
    
    * Fix lost change
    
    * I can't recall everything in this commit. Error handling for one
    
    * Wait for queue to be finished.
    
    Uses join() and task_done()
    
    * Pyflakes stuff
    
    * Cleaning up this PR for merge + making sense of things.
    
    - Poll info during poll is in `poll.json` while votes are in `votename_votes.json`
    - Poll info after vote is all moved to `Polls` folder with `poll.json` becoming `votename.json`
    - No longer writing empty dicts to files, just remove the damn thing.
    - Explicit str casting to prevent double votes
    - Swap out all `cmd` functions to their actual yknow, names?
    - Added spacing -> Compacted code might look good in low LOC, but it doesnt make it more readable.
    - Moved poll_ongoing boolean set to before moving the files, that prevents voters during the file move.
    - Poll tallies before closing.

--- addons/vote.py
+++ addons/vote.py
@@ -3,39 +3,143 @@ import json
 from discord.ext import commands
 import asyncio
 import os
+import shutil
 
 class Vote:
+    pollcfg = {}
     vote_list = {}
-    cleanup_msgs_after = 30
+    poll_ongoing = False
 
     def __init__(self, bot):
         self.bot = bot
-        if os.path.isfile("votes.json"):
-            with open("votes.json", "r") as votefile:
-                self.vote_list = json.load(votefile)
+        if os.path.isfile("poll.json"):
+            with open("poll.json", "r") as pollfile:
+                self.pollcfg = json.load(pollfile)
+                if len(self.pollcfg):
+                    self.poll_ongoing = True
+                    vote_name = "{}_votes.json".format(self.pollcfg["name"])
+                    if os.path.isfile(vote_name):
+                        with open(vote_name, "r") as votefile:
+                            self.vote_list = json.load(votefile)
         self.queue = asyncio.Queue()
+
+    async def is_poll_ongoing(ctx):
+        if ctx.cog.poll_ongoing:
+            print("h")
+            return True
+        else:
+            print("f")
+            raise PollException("This thing is a pain")
 
     async def process_vote(self):
         choice, ctx = await self.queue.get()
         if choice == "cancel":
             popped = self.vote_list.pop(ctx.author.id, None) # default param means nothing goes wrong if someone cancels again
             if popped is not None:
-                await ctx.send("Your vote has been cancelled!", delete_after=self.cleanup_msgs_after)
+                await ctx.send("Your vote has been cancelled!")
+        elif choice not in self.pollcfg["options"]:
+            await ctx.send("This is not a valid voting option.")
         else:
-            self.vote_list[ctx.author.id] = choice
-            await ctx.send(f"Your vote for {choice} has been succesfully registered!", delete_after=self.cleanup_msgs_after)
-        with open("votes.json", "w") as votefile:
+            self.vote_list[str(ctx.author.id)] = choice
+            await ctx.send(f"Your vote for {choice} has been succesfully registered!")
+        with open("{}.json".format("{}_votes".format(self.pollcfg["name"])), "w") as votefile:
             json.dump(self.vote_list, votefile)
-        try:
-            await ctx.message.delete()
-        except:
-            pass
+        self.queue.task_done()
+
+    @commands.guild_only()
+    @commands.group()
+    async def poll(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send("I don't know what you want me to do. If you would like to create a poll, please do `$help poll create`. If you would like to close an ongoing poll, please do `$poll close`.")
+
+    @poll.command(alias='start')
+    # prob add evi final tally before finishing poll
+    async def create(self, ctx, link, name, *, options):
+        """To create a poll, you must provide the link to the imgur gallery, the name for the current poll, and a list of available options split by vertical bars. Ex:\n$poll create https://imgur.com/testing testingpoll2018 A | B | C | D"""
+        if self.poll_ongoing:
+            return await ctx.send("There's already a poll running. You must stop that one first.")
+        elif not link.startswith("https://"):
+            return await ctx.send(f"Your inputted link, `{link}`, is not a valid link.")
+
+        vote_options = []
+        for option in options.split(" | "):
+            vote_options.append(option)
+        if len(vote_options) < 2:
+            return await ctx.send("You can't have a poll with only one option.")
+
+        self.pollcfg["name"] = name
+        self.pollcfg["link"] = link
+        self.pollcfg["options"] = vote_options
+
+        with open("poll.json", "w") as pollfile:
+            json.dump(self.pollcfg, pollfile)
+
+        with open(f"{name}_votes.json", "w") as votefile:
+            json.dump(self.vote_list, votefile)
+
+        self.poll_ongoing = True
+        await ctx.send("Poll successfully created!")
+
+    @commands.check(is_poll_ongoing)
+    @poll.command(alias='end')
+    async def close(self, ctx):
+        await ctx.invoke(self.bot.get_command("tally"))
+        self.poll_ongoing = False # Should be _much_ earlier
+
+        votes = dict.fromkeys(self.pollcfg["options"], 0)
+
+        for vote in self.vote_list.values():
+            if vote in votes:
+                votes[vote] += 1
+
+        for v in votes.items():
+            self.pollcfg[v[0]] = v[1]
+
+        os.makedirs("Polls", exist_ok=True)
+        shutil.move("poll.json", "Polls/{}.json".format(self.pollcfg["name"]))
+        shutil.move("{}_votes.json".format(self.pollcfg["name"]), "Polls/{}_votes.json".format(self.pollcfg["name"]))
+
+        self.pollcfg = {}
+        self.vote_list = {}
+
+        os.remove("poll.json")
+
+        await ctx.send("Poll successfully closed!")
+
+    @poll.command()
+    @commands.check(is_poll_ongoing)
+    async def info(self, ctx):
+        embed = discord.Embed(title="Current poll")
+        embed.add_field(name="Poll name", value=self.pollcfg["name"])
+        embed.add_field(name="Link to gallery", value=self.pollcfg["link"])
+        embed.add_field(name="Options", value=" | ".join(self.pollcfg["options"]))
+        await ctx.send(embed=embed)
 
     @commands.guild_only()
     @commands.command()
+    @commands.check(is_poll_ongoing)
     async def vote(self, ctx, choice):
         await self.queue.put((choice, ctx)) # tuple -> immutable plus easy get
         await self.process_vote()
 
+    @commands.guild_only()
+    @commands.command()
+    @commands.check(is_poll_ongoing)
+    async def tally(self, ctx):
+        await self.queue.join()
+        embed = discord.Embed(title="Current tally of votes")
+
+        votes = dict.fromkeys(self.pollcfg["options"], 0)
+        for vote in self.vote_list.values():
+            if vote in votes:
+                votes[vote] += 1
+        for v in votes.items():
+            embed.add_field(name=f"{v[0]}", value=f"{v[1]}")
+
+        await ctx.send(embed=embed)
+        
 def setup(bot):
     bot.add_cog(Vote(bot))
+    
+class PollException(commands.errors.CommandError):
+    pass
--- main.py
+++ main.py
@@ -1,35 +1,55 @@
 from discord.ext import commands
 import config
+from addons.vote import PollException
 
-'''A simple bot framework. Based on https://gist.github.com/noirscape/cc8e65c1c42f26af0a9b3780e161817d'''
+"""A simple bot framework. Based on https://gist.github.com/noirscape/cc8e65c1c42f26af0a9b3780e161817d"""
 
-bot = commands.Bot(command_prefix='$', description='Voting bot for Nintendo Homebrew icon contests') 
+bot = commands.Bot(command_prefix="$", description="Voting bot for Nintendo Homebrew icon contests")
 
-addons = [
-    'addons.vote',
+modules = [
+    "addons.vote",
 ]
 
-for module in addons:
+for module in modules:
     try:
         bot.load_extension(module)
     except Exception as e:
-        print('Could not load module ' + module)
+        print("Could not load module " + module)
         print(e)
 
 @bot.event
 async def on_ready():
     bot.guild = bot.get_guild(config.guild)
-    print('Initialized on ' + bot.guild.name)
+    print("Initialized on " + bot.guild.name)
+    
+@bot.event
+async def on_command_error(ctx, e):
+    if isinstance(e, commands.errors.CommandNotFound):
+        pass # Don't need you in console
+    elif isinstance(e, commands.errors.CheckFailure) and not isinstance(e, PollException):
+        await ctx.send("You don't have permission to use this command!")
+    elif isinstance(e, PollException):
+        await ctx.send("There's no poll ongoing!")
+    elif isinstance(e, commands.errors.MissingRequiredArgument):
+        formatted_help = await commands.formatter.HelpFormatter().format_help_for(ctx, ctx.command)
+        await ctx.send(f"You're missing required arguments.\n{formatted_help[0]}")
+
+@bot.event
+async def on_error(event, *args, **kwargs):
+    if isinstance(args[0], commands.errors.CommandNotFound):
+        return
+
     
 @bot.command()
 async def reload(ctx):
-    for module in addons:
+    for module in modules:
         try:
             bot.unload_extension(module)
             bot.load_extension(module)
         except Exception as e:
-            await ctx.send('Could not reload module ' + module)
-            print('Could not load module ' + module)
+            await ctx.send("Could not reload module " + module)
+            print("Could not load module " + module)
             print(e)
+        await ctx.send("Reloading of modules finished!")
 
 bot.run(config.token)
